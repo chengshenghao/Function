2155e068bd91fc0893391b2284e8837f7c66b33e
chengshenghao <1151799682@qq.com>
2018-09-14 15:40:27 +0800

    new：新增点击添加或删除功能

--- app/src/main/java/com/csh/function/MainActivity.java
+++ app/src/main/java/com/csh/function/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.callback.ItemDragAndSwipeCallback;
 import com.chad.library.adapter.base.listener.OnItemDragListener;
 import com.csh.function.bean.FunctionBean;
@@ -17,14 +20,18 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<FunctionBean> arrayCommon;
     private ArrayList<FunctionBean> arrayOther;
     private RecyclerView rvOther;
+    private FunctionSetAdapter commonAdapter;
+    private FunctionSetAdapter otherAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
+
+
         //常用操作的拖拽
-        FunctionSetAdapter commonAdapter = new FunctionSetAdapter(arrayCommon);
+        commonAdapter = new FunctionSetAdapter(arrayCommon);
         ItemDragAndSwipeCallback commDragCallBack = new ItemDragAndSwipeCallback(commonAdapter);
         commDragCallBack.setSwipeMoveFlags(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
         ItemTouchHelper commTouch = new ItemTouchHelper(commDragCallBack);
@@ -44,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //全部操作的拖拽
-        FunctionSetAdapter otherAdapter = new FunctionSetAdapter(arrayOther);
+        otherAdapter = new FunctionSetAdapter(arrayOther);
         ItemDragAndSwipeCallback otherDragCallback = new ItemDragAndSwipeCallback(otherAdapter);
         otherDragCallback.setSwipeMoveFlags(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.DOWN | ItemTouchHelper.UP);
         ItemTouchHelper otherTouch = new ItemTouchHelper(otherDragCallback);
@@ -62,25 +69,57 @@ public class MainActivity extends AppCompatActivity {
         otherAdapter.setOnItemDragListener(listener1);
         rvOther.setAdapter(otherAdapter);
 
+
+        //点击事件处理
+        commonAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                changState(commonAdapter.getData().get(position));
+            }
+        });
+        otherAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                changState(otherAdapter.getData().get(position));
+            }
+        });
     }
 
     private void initData() {
         arrayCommon = new ArrayList<FunctionBean>();
-        for (int i = 0; i <4 ; i++) {
+        for (int i = 0; i < 4; i++) {
             FunctionBean functionBean = new FunctionBean();
-            functionBean.setId(i+1);
+            functionBean.setId(i + 1);
             functionBean.setIsSelected(1);
-            functionBean.setName("测试"+i);
+            functionBean.setName("测试" + i);
             arrayCommon.add(functionBean);
         }
         arrayOther = new ArrayList<FunctionBean>();
-        for (int i = 4; i <12 ; i++) {
+        for (int i = 4; i < 12; i++) {
             FunctionBean functionBean = new FunctionBean();
-            functionBean.setId(i+1);
+            functionBean.setId(i + 1);
             functionBean.setIsSelected(0);
-            functionBean.setName("测试"+i);
+            functionBean.setName("测试" + i);
             arrayOther.add(functionBean);
         }
+    }
 
+    public void changState(FunctionBean functionBean) {
+        if (functionBean.getIsSelected() == 1) {
+            functionBean.setIsSelected(0);
+            arrayCommon.remove(functionBean);
+            arrayOther.add(functionBean);
+        } else {
+            if (arrayCommon.size() >= 4) {
+                Toast.makeText(this, "常用功能最多选择4个", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            functionBean.setIsSelected(1);
+            arrayOther.remove(functionBean);
+            arrayCommon.add(functionBean);
+        }
+//        getmDatabase().functionDao().replace(dbFunction);
+        commonAdapter.notifyDataSetChanged();
+        otherAdapter.notifyDataSetChanged();
     }
 }
